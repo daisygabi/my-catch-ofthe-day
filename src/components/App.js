e13f6e8ccbbebca016ef09c4f83e436d0b9e200e
Gabi <gabrielaradu.developer@gmail.com>
2018-11-08 16:02:38 +0200

    Add todos in App, where it should have been.

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ class App extends Component {
     render() {
         return (
             <Header/>
+            // TODO Add a header to the page
+            // TODO Add the left sidebar where we keep all the fishes
+            // TODO Add middle bar where we count the shopping
+            // TODO Add right sidebar where we edit the fishes
         );
     }
 }
