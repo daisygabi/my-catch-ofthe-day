00c0ff0a5df40d9f10ec2df9881535be54c7daca
Gabi <gabrielaradu.developer@gmail.com>
2018-11-08 16:03:43 +0200

    First commit for tests.

---
+++ src/components/tests/StorePicker.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import StorePicker from '../StorePicker';
+
+it('renders without crashing', () => {
+    shallow(<StorePicker/>);
+});
+
+test('Show left sidebar', () => {
+
+});
