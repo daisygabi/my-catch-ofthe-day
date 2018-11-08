0c7f1b16e802b93562f4e2ee098fc272ca97ed75
Gabi <gabrielaradu.developer@gmail.com>
2018-11-08 15:43:35 +0200

    Add todos for main component.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class StorePicker extends React.Component {
     render () {
-        return("<div> Hi from store picker</div>");
+        return(
+            // TODO Add a header to the page
+            // TODO Add the left sidebar where we keep all the fishes
+            // TODO Add middle bar where we count the shopping
+            // TODO Add right sidebar where we edit the fishes
+            "<div> Hi from store picker</div>"
+        );
     }
 }
 export default StorePicker;
