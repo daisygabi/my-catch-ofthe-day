9a1747a95b61530c49f7d4ab53adecd3968d309f
Gabi <gabrielaradu.developer@gmail.com>
2018-11-08 15:29:16 +0200

    Insert Header component into App.

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,21 @@
 import React from "react";
+import logo from "./logo.svg";
+import './App.css';
 
-const Header = () => {
-   <div>header here</div>;
-};
+const Header = props => (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+                Edit code ffs
+            </p>
+            <a className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer">
+                Learn React
+            </a>
+        </header>
+    </div>
+);
 export default Header;
