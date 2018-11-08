bca0566344a1894d83a053d6444081ce893bc67f
Gabi <gabrielaradu.developer@gmail.com>
2018-11-08 15:41:48 +0200

    Update message to know where I am.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class StorePicker extends React.Component {
     render () {
-        return("<div> HI</div>");
+        return("<div> Hi from store picker</div>");
     }
 }
 export default StorePicker;
