c196c99c11327f15ed7e7bbc0dbc554091d1bc7f
Gabi <gabrielaradu.developer@gmail.com>
2018-11-08 16:32:26 +0200

    Add input and button in store picker with tests.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 export default class StorePicker extends React.Component {
-    render () {
-        return(
+    storeNameInput = React.createRef();
 
-            "<div> Hi from store picker</div>"
+    render() {
+        return (
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <div> Hi from store picker</div>
+                <h1>Please enter a name for a store</h1>
+                <input type="text" required placeholder="Store Name" ref={this.storeNameInput}/>
+                <button className="goto-store-button" type="submit">Go to store</button>
+            </form>
         );
     }
+
+    goToStore = (event) => {
+        // stop the form from clearing
+        event.preventDefault();
+        // Get the value from the form
+        const storeName = this.storeNameInput.current.value;
+        console.log(this.props);
+        // redirect to a different path with the name in url
+        this.props.history.push(`/store/${storeName}`);
+    };
 }
--- src/components/tests/StorePicker.test.js
+++ src/components/tests/StorePicker.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import StorePicker from '../StorePicker';
+import StorePicker from "../StorePicker";
 
-it('renders without crashing', () => {
-    shallow(<StorePicker/>);
-});
+describe('verify rendered UI items', () => {
+    it('find store name button', () => {
+        const wrapper = shallow(<StorePicker/>);
+        const storeButton = <button className="goto-store-button" type="submit">Go to store</button>;
+        expect(wrapper.contains(storeButton)).toEqual(true);
+    });
 
-test('Show left sidebar', () => {
-
+    it('find store name input name', () => {
+        const wrapper = shallow(<StorePicker/>);
+        const inputName = <input type="text" required placeholder="Store Name" />;
+        expect(wrapper.contains(inputName)).toEqual(true);
+    });
 });
