243e8d7d10d21ce1cd5e30602136a216db6f63b0
Gabi <gabrielaradu.developer@gmail.com>
2018-11-08 17:03:57 +0200

    Redirect user to a main component when the store name is given at input.

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,11 +2,13 @@ import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import NotFound from "./NotFound";
 import StorePicker from "./StorePicker";
+import App from "./App";
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={StorePicker}/>
+            <Route exact path="/store/:storeId" component={App}/>
             <Route component={NotFound}/>
         </Switch>
     </BrowserRouter>
