2277fbebebe2f4ef51f8957d3072c62b2357bf6c
Gabi <gabrielaradu.developer@gmail.com>
2018-11-12 19:56:24 +0200

    First commit to show and update order depending on fish status.

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,47 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {formatPrice} from "../helper";
 
 export default class Order extends React.Component {
+    static propTypes = {
+        fishes: PropTypes.object,
+        order: PropTypes.object
+    };
+
     render() {
-        return(
-            <div>Show Order here</div>
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+            console.log(prevTotal + " / fish name: " + fish.name + ", count: " + count);
+            const isAvailable = fish && fish.status === 'available';
+            if (isAvailable) {
+                return prevTotal + (count * fish.price);
+            }
+            return prevTotal;
+        }, 0);
+
+
+        return (
+            <div>
+                <h2>Order</h2>
+                <ul>
+                    {orderIds.map(this.renderOrder)}
+                </ul>
+                <div>
+                    Total: {total}
+                </div>
+            </div>
         );
     }
+
+    renderOrder = (key) => {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        const isAvailable = fish.status === 'available';
+        if (!isAvailable) {
+            return <li>Sorry {fish ? fish.name : 'fish'} is not available</li>;
+        }
+        return <li>{count} lbs {fish.name} {formatPrice(count * fish.price)}</li>;
+    };
 }
