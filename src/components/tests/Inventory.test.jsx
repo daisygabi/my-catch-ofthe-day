9f3dff78846c307af2486fb5301cbfddffad8b11
Gabi <gabrielaradu.developer@gmail.com>
2018-11-12 13:31:11 +0200

    Test passing data and set it on state in inventory.

--- src/components/tests/Inventory.test.jsx
+++ src/components/tests/Inventory.test.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import Inventory from '../Inventory';
 
 describe('verify rendered UI items', () => {
+    var fish;
+    var component;
+
+    beforeEach(() => {
+        fish = {
+            name: "Pacific Halibut",
+            image: "/images/hali.jpg",
+            desc:
+                "Everyones favorite white fish. We will cut it to the size you need and ship it.",
+            price: 1724,
+            status: "available"
+        };
+    });
+    beforeEach(() => {
+        component = mount(<Inventory loadSamplesFishes={fish}/>);
+    });
+
     it('renders without crashing', () => {
         shallow(<Inventory/>);
+    });
+    it('sends fishes data to props', () => {
+        expect(component).toBeTruthy();
+        expect(component.find('#loadFishesButton').length).toEqual(1);
+
+        // Click on submit button
+        const fakeEvent = Object.assign(jest.fn(), {
+            preventDefault() {
+            }
+        });
+        component.find('#loadFishesButton').simulate('click', fakeEvent);
+        expect(component.props().loadSamplesFishes).toEqual(fish);
     });
 });
