8673aa4debfa392b161dc1195db138dbea430f51
Gabi <gabrielaradu.developer@gmail.com>
2018-11-12 13:30:44 +0200

    Add possibility to add sample fishes from a file.

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,28 @@ import Header from "./Header";
 import Fishes from "./Fishes";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 export default class App extends Component {
+    state = {
+        fishes: {}
+    };
+
     render() {
         return (
             <div className="App">
                 <Header/>
                 <Fishes/>
                 <Order/>
-                <Inventory/>
+                <Inventory loadSampleFishes={this.loadSamplesFishes}/>
             </div>
         );
     }
 
     componentDidMount() {
-
     }
+
+    loadSamplesFishes = () => {
+        this.setState({fishes: sampleFishes});
+    };
 }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default class Inventory extends React.Component {
+    static propTypes = {
+        loadSampleFishes: PropTypes.func
+    };
+
     render() {
-        return(
-            <div>Show Inventory here</div>
+        return (
+            <div>
+                <p>Show Inventory here</p>
+                <button id="loadFishesButton"
+                        onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
+            </div>
         );
     }
 }
